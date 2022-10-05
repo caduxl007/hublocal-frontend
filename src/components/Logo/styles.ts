767f74c32996336f8d7987a0b9a1dfa96441c458
caduxl007 <eduardo2020dev@gmail.com>
2022-10-05 17:29:00 -0300

    feat: create component Logo

---
+++ src/components/Logo/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+import logo from '../../assets/logo.png';
+
+export const Container = styled.div`
+  width: 13rem;
+  height: 5rem;
+  background-size: 100% auto;
+  background-image: url(${logo});
+  background-repeat: no-repeat;
+`;
